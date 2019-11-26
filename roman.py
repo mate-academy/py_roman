52eb5455250a5086b9ef134c5e4f429b531e8b52
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-26 10:48:38 +0200

    1st commit

--- roman.py
+++ roman.py
@@ -1,3 +1,25 @@
-def convert(s: str) -> int:
+"""docstring"""
 
-    return 42
+
+def convert(string: str) -> int:
+    """convert roman to int"""
+    roman = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50
+    }
+    output = 0
+    for i, char in enumerate(string):
+        try:
+            if roman[char] < roman[string[i + 1]]:
+                output -= roman[char]
+            elif roman[char] > roman[string[i + 1]]:
+                output += roman[char]
+            else:
+                output += roman[char]
+        except IndexError:
+            output += roman[char]
+            return output
+
+    return output
