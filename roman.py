76a5fa0b4f0151070086265703fffcaf7b9add4d
vlysenok <tina.lysenok@gmail.com>
2019-11-26 12:49:19 +0200

    hw7_py_roman

--- roman.py
+++ roman.py
@@ -1,3 +1,22 @@
-def convert(s: str) -> int:
+"""
+module convert
+"""
 
-    return 42
+
+def convert(roman_num: str) -> int:
+    """
+    Conversion roman number to integer.
+    :param roman_num: str
+    :return: int
+    """
+    result = 0
+    roman_num = roman_num.upper()
+    rome_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    for i, _ in enumerate(roman_num):
+        if i + 1 < len(roman_num) and rome_dict[roman_num[i]] < rome_dict[roman_num[i + 1]]:
+            result -= rome_dict[roman_num[i]]
+        else:
+            result += rome_dict[roman_num[i]]
+
+    return result
