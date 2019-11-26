d050e0339d6cf67ed722056b1ac0088f5d5a3b83
logotip <logotip123@yahoo.com>
2019-11-26 11:14:49 +0200

    v 1.0

--- roman.py
+++ roman.py
@@ -1,3 +1,31 @@
-def convert(s: str) -> int:
+"""Convert roman digits to arabian module"""
 
-    return 42
+
+def convert(strng: str) -> int:
+    """
+    :param strng: string with roman digits
+    :return: arabian result
+    """
+    roman_digits = {"I": 1, "V": 5, "X": 10, "L": 50}
+    result = 0
+    index = 0
+    strng = strng[::-1]
+    while index < len(strng):
+        try:
+            try:
+                if roman_digits[strng[index]] == roman_digits[strng[index+1]] \
+                        == roman_digits[strng[index+2]]:
+                    result += roman_digits[strng[index]]*3
+                    index += 3
+                    continue
+            except IndexError:
+                pass
+            if roman_digits[strng[index]] > roman_digits[strng[index+1]]:
+                result += roman_digits[strng[index]] - roman_digits[strng[index+1]]
+            else:
+                result += roman_digits[strng[index]] + roman_digits[strng[index+1]]
+            index += 2
+        except IndexError:
+            result += roman_digits[strng[index]]
+            index += 1
+    return result
