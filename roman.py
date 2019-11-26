f26b5626857410e6c20536556581360e417ed420
Senku1 <morhaliuk@gmail.com>
2019-11-26 11:56:51 +0200

    Solved_2

--- roman.py
+++ roman.py
@@ -1,4 +1,5 @@
 """doc-sting"""
+from typing import List
 
 
 def convert(roman: str) -> int:
@@ -7,9 +8,10 @@ def convert(roman: str) -> int:
     :param roman: str
     :return: int
     """
-    rom_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50}
+    rom_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+               'C': 100, 'D': 500, 'M': 1000}
     lst_rom = list(roman)
-    integers = []
+    integers: List[int] = []
     while lst_rom:
         if not integers or integers[-1] >= rom_int[lst_rom[0]]:
             integers.append(rom_int[lst_rom.pop(0)])
