230d17fb7467fc86531cc244100667cdf3d347cb
Senku1 <morhaliuk@gmail.com>
2019-11-26 11:21:55 +0200

    Solved

--- roman.py
+++ roman.py
@@ -1,3 +1,20 @@
-def convert(s: str) -> int:
+"""doc-sting"""
+from typing import List
 
-    return 42
+
+def convert(roman: str) -> int:
+    """
+    Converts roman number to integer
+    :param roman: str
+    :return: int
+    """
+    rom_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50}
+    lst_rom = list(roman)
+    integers: List[int] = []
+    while lst_rom:
+        if not integers or integers[-1] >= rom_int[lst_rom[0]]:
+            integers.append(rom_int[lst_rom.pop(0)])
+        else:
+            integers[-1] = rom_int[lst_rom.pop(0)] - integers[-1]
+
+    return sum(integers)
