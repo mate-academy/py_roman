ae200a0360906fe2df76e17ee453b98d274bb8ce
Senku1 <morhaliuk@gmail.com>
2019-11-27 12:54:12 +0200

    Solved_3

--- roman.py
+++ roman.py
@@ -11,7 +11,7 @@ def convert(roman: str) -> int:
     rom_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                'C': 100, 'D': 500, 'M': 1000}
     lst_rom = list(roman)
-    integers: List[int] = []
+    integers = []            # type: List[int]
     while lst_rom:
         if not integers or integers[-1] >= rom_int[lst_rom[0]]:
             integers.append(rom_int[lst_rom.pop(0)])
