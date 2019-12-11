2b1924c843e212876c745d7b4f8cd50aba9990c2
zaikovlad95@gmail.com <Makepeacenotwar1995>
2019-12-12 01:13:08 +0200

    Convert roman into integer.

--- roman.py
+++ roman.py
@@ -4,12 +4,12 @@
 def convert(input_str: str) -> int:
     """get roman string and return an integer"""
     roman_values = {"I": 1, "V": 5, "X": 10, "L": 50}
-    sum_int_value = 0
+    int_value = 0
     for rom_ind, rom_val in enumerate(input_str):
         if rom_ind > 0 and \
                 roman_values[rom_val] > roman_values[input_str[rom_ind - 1]]:
-            sum_int_value += roman_values[rom_val] - \
+            int_value += roman_values[rom_val] - \
                                      2 * roman_values[input_str[rom_ind - 1]]
         else:
-            sum_int_value += roman_values[rom_val]
-    return sum_int_value
+            int_value += roman_values[rom_val]
+    return int_value
